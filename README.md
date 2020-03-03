898c9f997033e9b45153ee436a91a99cc0f62778
GH0st3rs <GH0st3rs@users.noreply.github.com>
2020-03-03 13:12:25 +0300

    Add files and update README

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # iot_detector
-Multithreaded scanner for detecting IoT devices
+A multithreaded scanner for detecting IoT devices, which allows you to identify a device on the Internet based on a specific request to it. This allows you to more accurately determine the device and avoid false positives or honeypot
+
+
+## Usage
+```bash
+Usage of ./iot_detector:
+  -l string
+        List of ip,port
+  -r string
+        Json request file
+  -t int
+        Thread count (default 1000)
+  -v    Verbose
+```
+
+
+## JSON request format
+```json
+{
+    "path": "/cgi-bin/target.cgi",
+    "method": "POST",
+    "headers": {
+        "Content-Type": "text/xml; charset=utf-8",
+        "X-Requested-With": "XMLHttpRequest"
+    },
+    "data": "action=get_device_info&method=1",
+    "search": "<DeviceName>DIR-123</DeviceName>"
+}
+```
+
+* `path` - A specific URL, when accessed, returns the model or version of the device
+
+* `method` - Request method GET or POST
+
+* `headers` - Request header
+
+* `data` - Post request body
+
+* `search` - Success detection pattern
+
+
+## Run or install
+```bash
+$ go build $(pwd)
+$ ./iot_detector ...
+```
---
+++ main.go
@@ -0,0 +1,146 @@
+// iot_detector project main.go
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+)
+
+type ListItem struct {
+	ip   string
+	port string
+}
+
+type Request struct {
+	Path    string            `json:"path"`
+	Method  string            `json:"method"`
+	Headers map[string]string `json:"headers"`
+	Search  string            `json:"search"`
+	Data    string            `json:"data"`
+}
+
+var THREADS_COUNT int
+var VERBOSE bool
+var GLOBAL_REQUEST Request
+
+func load_request_from_file(filename string) error {
+	jsonFile, err := os.Open(filename)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	// Parse Json file
+	err = json.Unmarshal(byteValue, &GLOBAL_REQUEST)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func request(target string, port string, shema string) (string, bool) {
+	if shema == "https" {
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	// build a new request, but not doing the POST yet
+	url := fmt.Sprintf("%s://%s:%s%s", shema, target, port, GLOBAL_REQUEST.Path)
+	req, err := http.NewRequest(GLOBAL_REQUEST.Method, url, bytes.NewBuffer([]byte(GLOBAL_REQUEST.Data)))
+	if err != nil {
+		return "[ERROR CONNECT]", false
+	}
+	// Insert header items
+	for header := range GLOBAL_REQUEST.Headers {
+		req.Header.Add(header, GLOBAL_REQUEST.Headers[header])
+	}
+	// now POST it
+	resp, err := client.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		return "[NO RESPONSE]", false
+	}
+	defer resp.Body.Close()
+	answer, err := ioutil.ReadAll(resp.Body)
+	if strings.Contains(string(answer), GLOBAL_REQUEST.Search) {
+		return "SUCCESS", true
+	}
+	return "[NOT DETECTED]", false
+}
+
+func worker(thread_num int, jobs chan ListItem, wg *sync.WaitGroup) {
+	for v := range jobs {
+		status := false
+		answer := ""
+		for _, shema := range []string{"https", "http"} {
+			answer, status = request(v.ip, v.port, shema)
+			if status == true {
+				break
+			}
+		}
+		if status == true || VERBOSE == true {
+			fmt.Printf("{%d}\t%s\t%s\t%s\n", thread_num, v.ip, v.port, answer)
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	var iplist string
+	var request_file string
+	flag.IntVar(&THREADS_COUNT, "t", 1000, "Thread count")
+	flag.StringVar(&iplist, "l", "", "List of ip,port")
+	flag.StringVar(&request_file, "r", "", "Json request file")
+	flag.BoolVar(&VERBOSE, "v", false, "Verbose")
+	flag.Parse()
+
+	numcpu := runtime.NumCPU()
+	fmt.Println("NumCPU", numcpu)
+	runtime.GOMAXPROCS(numcpu)
+
+	err := load_request_from_file(request_file)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	jobs := make(chan ListItem, 100)
+	wg := sync.WaitGroup{}
+	for i := 1; i < THREADS_COUNT; i++ {
+		go worker(i, jobs, &wg)
+		wg.Add(1)
+	}
+
+	file, err := os.Open(iplist)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitted_data := strings.Split(line, ",")
+		item := ListItem{ip: splitted_data[0], port: splitted_data[1]}
+		jobs <- item
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	close(jobs)
+	wg.Wait()
+}
